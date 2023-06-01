5a51925f0e6f6d33274830e24ced849965489928
Jose-WDG <neto.silvacem@gmail.com>
2023-06-01 01:33:02 -0300

    Organizando pastas.

--- src/components/FreelancerList.jsx
+++ src/components/FreelancerList.jsx
@@ -37,50 +37,40 @@ const FreelancerRecentWork = styled.p`
   color: var(--second-text-color);
   margin-bottom: 0;
 `;
+const StyledLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 5px;
+  margin: 5px;
+  &:hover {
+    background-color: #ffffff75;
+    text-decoration: underline;
+  }
+`;
 
-const FreelancerList = () => {
-  const freelancers = [
-    {
-      id: 1,
-      name: 'José Ramalho',
-      photo: '/src/assets/foto-jose.jpg',
-      technologies: [
-       'Android', 'Java', 'Kotlin' , 'teste' , 'test', 'test'
-      ],
-      recentWork: 'BRQ digital solutions',
-    },
-    {
-      id: 2,
-      name: 'Felipe Araújo',
-      photo: '/src/assets/foto-felipe.png',
-      technologies: ['Swift', 'Dart', 'Fluter'],
-      recentWork: 'Project C, Project D',
-    },
-    // Add more freelancers as needed
-  ];
-
-  return (
-    <FreelancerListContainer>
-      {freelancers.map((freelancer, index) => (
-        <FreelancerCard key={index}>
-          <FreelancerName>{freelancer.name}</FreelancerName>
-          <FreelancerImage src={freelancer.photo} alt={freelancer.name} />
-          <FreelancerTechnologies>
-            Tecnologias: 
-            {freelancer.technologies.map((t,i) => (
-              <SkillItem>{t}</SkillItem>
-            ))}
-          </FreelancerTechnologies>
-          <FreelancerRecentWork>
-            Trabalho: {freelancer.recentWork}
-          </FreelancerRecentWork>
-          <Link to={`/freelancer/${freelancer.id}`}>
-    Ver detalhes do freelancer
-  </Link>
-        </FreelancerCard>
-      ))}
-    </FreelancerListContainer>
-  );
+return (
+  <FreelancerListContainer>
+    {freelancers.map((freelancer, index) => (
+      <FreelancerCard key={index}>
+        <FreelancerName>{freelancer.name}</FreelancerName>
+        <FreelancerImage src={freelancer.photo} alt={freelancer.name} />
+        <FreelancerTechnologies>
+          Tecnologias:
+          {freelancer.technologies.map((t, i) => (
+            <SkillItem>{t}</SkillItem>
+          ))}
+        </FreelancerTechnologies>
+        <FreelancerRecentWork>
+          Trabalho: {freelancer.recentWork}
+        </FreelancerRecentWork>
+        <StyledLink to={`/freelancer/${freelancer.id}`}>
+          Ver detalhes do freelancer
+        </StyledLink>
+      </FreelancerCard>
+    ))}
+  </FreelancerListContainer>
+);
 };
 
 export default FreelancerList;

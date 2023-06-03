17c706a26eab926709bf9851f4e208113606078e
Jose-WDG <neto.silvacem@gmail.com>
2023-06-03 00:17:58 -0300

    Removendo import não utilizado.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import FreelancerListComponent from '../components/FreelancerListComponent'
 import ContainerComponent from '../components/ContainerComponent'
 import NavbarComponen from '../components/NavbarComponent'
 import React from 'react'
 
 function Home() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <NavbarComponen />
